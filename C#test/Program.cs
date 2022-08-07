fba680d488ee8bf0953b3c461ac3520da5d335bd
Bala S <bala.s@collabera.com>
2022-08-07 11:52:51 +0530

    data type

---
+++ C#test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // // boolean data type
+            // bool isAdult = true;
+
+            // if (isAdult = true)
+            // {
+            //     Console.WriteLine("Adult is correct");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("not an Adult");
+            // }
+            string name = "Bala";
+            int age = 25;
+            System.Console.WriteLine("my name is: {0} and my age is {1}",name,age);
+            System.Console.WriteLine($"My name is {name}, and my age is {age}");
+        }
+    }
+}
